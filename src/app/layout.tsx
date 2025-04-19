c1a0e425aaf12359e1585f800a5606971eaf62a4
Jhojan Ropero <jhojanropero0@gmail.com>
2025-04-18 23:11:20 -0500

    chore: Initial Next.js setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata, Viewport } from "next";
+import { Tektur } from "next/font/google";
+import "./globals.css";
+
+const tektur = Tektur({
+  variable: "--font-tektur",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "404 Tech",
+  description: "An technology store"
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#15A448' }, // Original en claro
+    { media: '(prefers-color-scheme: dark)', color: '#0E7A36' }, // Versión más oscura
+  ],
+  colorScheme: 'light dark'
+}
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+  return (
+    <html lang="en">
+      <body className={`${tektur.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
