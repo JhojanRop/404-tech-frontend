cd39220c529280655fa302ea002cbe212fe4e1d4
alejandro <alejandrohurtadoalv@gmail.com>
2025-04-23 09:30:04 -0500

    Test de logos

--- src/components/home/brands.tsx
+++ src/components/home/brands.tsx
@@ -2,7 +2,7 @@ const Brands = () => {
     return (
         <div className="grid grid-cols-5 gap-4 py-12 px-8 w-full justify-center items-center h-[40vh] bg-black">
             <div>
-                <img src="/image-test.jpg" alt="brand" />
+                <img src="/logos/hp.svg" alt="logo_hp" />
             </div>
             <div>
                 <img src="/image-test.jpg" alt="brand" />
