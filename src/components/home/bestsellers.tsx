c88478174c84ba30b56dcecec7d8c7aaa3568262
Jhojan Ropero <jhojanropero0@gmail.com>
2025-04-25 22:35:08 -0500

    refactor: BestSeller page

--- src/components/home/bestsellers.tsx
+++ src/components/home/bestsellers.tsx
@@ -1,39 +1,28 @@
+'use client'
+import { useState } from "react";
+import ContentLayout from "../ContentLayout";
 import Card from "../ui/Card";
 
 const Bestsellers = () => {
+    const [products, setProducts] = useState([
+        { id: "A", image: "/image-test.jpg", title: 'Product A', price: 700, oldPrice: 0, isSale: false },
+        { id: "B", image: "/image-test.jpg", title: 'Product B', price: 326.99, oldPrice: 450.30, isSale: true },
+        { id: "C", image: "/image-test.jpg", title: 'Product C', price: 1500, oldPrice: 1700, isSale: true },
+        { id: "D", image: "/image-test.jpg", title: 'Product D', price: 314, oldPrice: 0, isSale: false },
+    ])
+
     return (
-        <div className="flex flex-col items-center justify-center gap-8 min-h-[70vh] bg-[#1c1c1c]">
-            <div className="flex flex-col items-start w-3/4">
-                <h2 className="font-bold text-2xl">BEST SELLERS</h2>
-            </div>
-            <div className="w-3/4">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4" >
-                    <Card
-                        image="/image-test.jpg"
-                        title="Product 1"
-                        price="$10.00"
-                        sale={true}
-                    />
-                    <Card
-                        image="/image-test.jpg"
-                        title="Product 2"
-                        price="$20.00"
-                        sale={false}
-                    />
-                    <Card
-                        image="/image-test.jpg"
-                        title="Product 3"
-                        price="$30.00"
-                        sale={false}
-                    />
-                    <Card
-                        image="/image-test.jpg"
-                        title="Product 4"
-                        price="$40.00"
-                        sale={false}
-                    />
+        <div className="bg-onyx">
+            <ContentLayout height={80} className="flex flex-col justify-center">
+                <h2 className="py-4 text-2xl font-bold">Best Sellers</h2>
+                <div>
+                    <div className="w-full flex justify-between">
+                        {products.map(product => (
+                            <Card key={product.id} image={product.image} title={product.title} sale={product.isSale} price={product.price.toLocaleString('es-ES')} />
+                        ))}
+                    </div>
                 </div>
-            </div>
+            </ContentLayout>
         </div>
     )
 }
