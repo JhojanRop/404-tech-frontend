37ffe8731681128e2bfa7c3fadde632598ed24a0
Jhojan Ropero <jhojanropero0@gmail.com>
2025-04-25 22:43:37 -0500

    refactor: update Newsletter section

--- src/components/home/newsletter.tsx
+++ src/components/home/newsletter.tsx
@@ -1,21 +1,19 @@
-import Button from "../ui/Button";
+'use client'
+import ContentLayout from "../ContentLayout";
 
 const Newsletter = () => {
     return (
-        <div className="flex flex-col min-h-[50vh] w-full justify-center items-center gap-4 bg-black">
-            <div>
-                <h2 className="font-bold text-xl lg:text-2xl">Subscribe to our newsletter</h2>
-            </div>
-            <div>
-                <p className="text-[15px] md:text-xl">Get 10% off your first purchare and exclusive deals</p>
-            </div>
-            <div className="flex">
-                <input type="text" placeholder="Email" className="border px-2" />
-                <Button variant="classic" className="px-4 py-2 rounded-none">
-                    <span>SEND</span>
-                </Button>
-            </div>
-        </div>
+        <section>
+            <ContentLayout height={50} className="flex flex-col justify-center text-center">
+                <h2 className="font-semi text-2xl">Subscribe to our newsletter</h2>
+                <p className="text-porcelain">Get 10% off your first purchare and exclusive deals</p>
+
+                <div className="flex justify-center gap-4 my-4">
+                    <input type="text" placeholder="Email" className="border border-white px-4 py-1 max-w-lg flex-grow" />
+                    <button className="bg-viridian-600 px-5">Send</button>
+                </div>
+            </ContentLayout>
+        </section>
     )
 }
 
