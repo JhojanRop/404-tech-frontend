7e0b8356e594d3f6e41d3a7f073135cf3d12b351
Jhojan Ropero <jhojanropero0@gmail.com>
2025-04-25 23:16:27 -0500

    refactor: refactor footer section

--- src/components/home/footer.tsx
+++ src/components/home/footer.tsx
@@ -1,53 +1,51 @@
+import ContentLayout from "../ContentLayout";
+
 const Footer = () => {
     return (
-        <div className="bg-[#1c1c1c] min-h-[60vh] flex flex-col items-center justify-center gap-8 py-8">
-            <div className="w-full px-4 flex flex-col lg:flex-row items-center lg:items-start justify-between gap-8">
-                {/* Logo */}
-                <div className="w-full lg:w-auto flex justify-center">
-                    <img src="/image-test.jpg" alt="footer" className="w-32" />
-                </div>
-
-                {/* Secciones de links */}
-                <div className="flex flex-col sm:flex-row justify-center lg:justify-end gap-6 w-full lg:w-2/3 text-center lg:text-left">
-                    {/* Grupo 1 */}
-                    <div className="flex flex-col gap-1">
-                        <h2 className="font-bold text-white">CUSTOMER SERVICE</h2>
-                        <a href="#">ORDER TRACKING</a>
-                        <a href="#">RETURN EXCHANGES</a>
-                        <a href="#">PAYMENT METHODS</a>
-                        <a href="#">FAQS</a>
+        <footer className="bg-onyx">
+            <div className="min-h-[70dvh] w-11/12 mx-auto flex items-center">
+                <div className="w-full h-full md:h-1/2 flex gap-y-5 justify-center md:justify-between items-stretch flex-col-reverse md:flex-row">
+                    {/* Logo */}
+                    <div className="w-full lg:w-auto flex justify-center">
+                        <img src="/image-test.jpg" alt="footer" className="w-32" />
                     </div>
 
-                    {/* Línea divisoria solo visible en pantallas grandes */}
-                    <div className="hidden lg:block bg-white w-[2px]"></div>
+                    {/* Secciones de links */}
+                    <div className="flex flex-col text-center md:text-left md:flex-row [&>div>a]:underline [&>div]:even:border-y-2 [&>div]:md:even:border-x-2 [&>div]:md:even:border-y-0">
+                        {/* Grupo 1 */}
+                        <div className="px-7 py-5 md:py-0 flex flex-col justify-evenly gap-1 text-sm md:text-base">
+                            <h2 className="font-semi text-lg md:text-xl text-white">CUSTOMER SERVICE</h2>
+                            <a href="#">ORDER TRACKING</a>
+                            <a href="#">RETURN EXCHANGES</a>
+                            <a href="#">PAYMENT METHODS</a>
+                            <a href="#">FAQS</a>
+                        </div>
 
-                    {/* Grupo 2 */}
-                    <div className="flex flex-col gap-1">
-                        <h2 className="font-bold text-white">ABOUT US</h2>
-                        <a href="#">WHO WE ARE</a>
-                        <a href="#">OUR SERVICES</a>
-                        <a href="#">LOCATION</a>
-                        <a href="#">CONTACT US</a>
-                    </div>
+                        {/* Grupo 2 */}
+                        <div className="px-10 py-5 md:py-0 flex flex-col justify-evenly gap-1 text-sm md:text-base">
+                            <h2 className="font-semi text-lg md:text-xl text-white">ABOUT US</h2>
+                            <a href="#">WHO WE ARE</a>
+                            <a href="#">OUR SERVICES</a>
+                            <a href="#">LOCATION</a>
+                            <a href="#">CONTACT US</a>
+                        </div>
 
-                    <div className="hidden lg:block bg-white w-[2px]"></div>
-
-                    {/* Grupo 3 */}
-                    <div className="flex flex-col gap-1">
-                        <h2 className="font-bold text-white">CATEGORIES</h2>
-                        <a href="#">LAPTOPS & COMPUTERS</a>
-                        <a href="#">GAMING & ACCESORIES</a>
-                        <a href="#">MONITORS & PHERIPHERICALS</a>
-                        <a href="#">COMPONENTS & UPGRADES</a>
+                        {/* Grupo 3 */}
+                        <div className="px-7 py-5 md:py-0 flex flex-col justify-evenly gap-1 text-sm md:text-base">
+                            <h2 className="font-semi text-lg md:text-xl text-white">CATEGORIES</h2>
+                            <a href="#">LAPTOPS & COMPUTERS</a>
+                            <a href="#">GAMING & ACCESORIES</a>
+                            <a href="#">MONITORS & PHERIPHERICALS</a>
+                            <a href="#">COMPONENTS & UPGRADES</a>
+                        </div>
                     </div>
                 </div>
             </div>
-
             {/* Copyright */}
             <div className="bg-black w-full py-4 text-xs text-center">
                 <p>© 2025 404TECH ALL RIGHTS RESERVED.</p>
             </div>
-        </div>
+        </footer>
 
     )
 }
