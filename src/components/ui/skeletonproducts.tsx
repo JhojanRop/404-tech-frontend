84fc80a04c048b954c8c9e6d9f3efbc773e6cb86
Jhojan Ropero <jhojanropero0@gmail.com>
2025-05-06 00:40:01 -0500

    feat: update product skeleton

--- src/components/ui/skeletonproducts.tsx
+++ src/components/ui/skeletonproducts.tsx
@@ -1,10 +1,25 @@
+import { StarIcon } from "@heroicons/react/24/solid";
+
 export default function SkeletonProduct() {
     return (
-        <div className="animate-pulse bg-white rounded-2xl shadow p-4 space-y-4">
-            <div className="bg-gray-200 h-40 rounded-xl" />
-            <div className="h-4 bg-gray-200 rounded w-3/4" />
-            <div className="h-4 bg-gray-200 rounded w-1/2" />
-            <div className="h-6 bg-gray-300 rounded w-1/3 mt-2" />
+        <div className="animate-pulse group relative text-foreground border-r border-b border-gray-200 p-4 sm:p-6">
+            <div className="bg-gray-200 w-full mx-auto aspect-square rounded-lg object-cover" />
+            <div className="pt-10 pb-4">
+                <div className="h-5 my-1 mx-auto bg-gray-200 rounded-sm w-full" />
+                <div className="mt-3 flex flex-col items-center">
+                    <div className="flex items-center mb-1">
+                        {[0, 1, 2, 3, 4].map((rating) => (
+                            <StarIcon
+                                key={rating}
+                                aria-hidden="true"
+                                className="text-gray-200 size-5 shrink-0 animate-pulse"
+                            />
+                        ))}
+                    </div>
+                    <div className="h-5 mx-auto bg-gray-300 rounded w-1/3" />
+                </div>
+                <div className="h-6 mx-auto mt-4 bg-gray-300 rounded w-1/2"></div>
+            </div>
         </div>
     );
 }
